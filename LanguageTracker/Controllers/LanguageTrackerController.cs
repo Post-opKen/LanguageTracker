3b608f8ca6129363838ce80763867e6589f761a4
Mike <mprelesnik@mail.greenriver.edu>
2019-05-21 11:46:44 -0700

    Added comments to pages

--- LanguageTracker/Controllers/LanguageTrackerController.cs
+++ LanguageTracker/Controllers/LanguageTrackerController.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+/**
+ * This class sets up the views/routes for the web app
+ * */
 namespace LanguageTracker.Controllers
 {
     public class LanguageTrackerController : Controller
     {
+		//default route
         public IActionResult Index()
         {
             return View();
         }
 
+		//individual students route
         public IActionResult Students()
         {
 			return View();
