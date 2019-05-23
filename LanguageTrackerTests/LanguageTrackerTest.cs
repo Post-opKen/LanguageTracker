f456b85e44ee3b7f6b31f738fb787703a90473b7
Mike <mprelesnik@mail.greenriver.edu>
2019-05-23 13:03:33 -0700

    Created unit testing shell

---
+++ LanguageTrackerTests/LanguageTrackerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LanguageTrackerTests
+{
+	[TestClass]
+	public class LanguageTrackerTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
