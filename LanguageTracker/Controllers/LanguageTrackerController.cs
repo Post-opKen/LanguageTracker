9fb69c6468b82134499b380b0c58e35ab28b8b85
Ean Daus <Eandaus123@gmail.com>
2019-05-24 23:21:06 -0700

    Can print the plain text of an uploaded csv file

--- LanguageTracker/Controllers/LanguageTrackerController.cs
+++ LanguageTracker/Controllers/LanguageTrackerController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
 
 /**
  * This class sets up the views/routes for the web app
@@ -7,22 +10,32 @@ namespace LanguageTracker.Controllers
 {
     public class LanguageTrackerController : Controller
     {
-		//default route
+        //default route
         public IActionResult Index()
         {
             return View();
         }
 
-		//individual students route
+        //individual students route
         public IActionResult Students()
         {
-			return View();
+            return View();
         }
 
         //excel upload route
         public IActionResult Upload()
         {
             return View();
+        }
+
+        //import CSV file from the Upload route
+        [HttpPost]
+        public string Import(IFormFile excelFile)
+        {
+            Stream stream = excelFile.OpenReadStream();
+            StreamReader reader = new StreamReader(stream);
+            string output = reader.ReadToEnd();
+            return output;
         }
     }
 }
